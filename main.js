109fdb97a3c8c41016aefa61e51a3e3c9d767b32
Mark6 <grigorash-mark@mail.ru>
2026-07-29 21:34:22 +0700

    оптимизация кода

--- main.js
+++ main.js
@@ -1,7 +1,4 @@
-let device;
-let context;
-let pipeline;
-let vertexBuffer;
+let device, context, pipeline, vertexBuffer;
 
 function render() {
     const texture = context.getCurrentTexture();
@@ -9,19 +6,15 @@ function render() {
 
     // Render Pass
     const pass = encoder.beginRenderPass({
-        colorAttachments:
-            [
-                {
-                    view: texture.createView(),
-                    clearValue: { r: 0.1, g: 0.2, b: 0.8, a: 1 },
-                    loadOp: "clear",
-                    storeOp: "store"
-                }
-            ]
+        colorAttachments: [{
+            view: texture.createView(),
+            clearValue: { r: 0.1, g: 0.2, b: 0.8, a: 1 },
+            loadOp: "clear",
+            storeOp: "store"
+        }]
     });
 
     pass.setPipeline(pipeline);
-    pass.setVertexBuffer(0, vertexBuffer);
     pass.draw(3);
     pass.end();
 
@@ -32,7 +25,8 @@ function render() {
     requestAnimationFrame(render);
 }
 
-async function main() {
+/** Инициализация WebGPU*/
+async function initWebGPU() {
     if (!navigator.gpu) {
         alert("WebGPU не поддерживается");
         return;
@@ -42,15 +36,24 @@ async function main() {
     if (!adapter) throw new Error("Не удалось получить GPU Adapter.");
 
     device = await adapter.requestDevice();
+    device.lost.then(() => { throw new Error("Не удалось получить GPU Device.") });
 
     const canvas = document.getElementById("canvas");
-    const format = navigator.gpu.getPreferredCanvasFormat();
+    // для четкости вывода
+    const devicePixelRatio = window.devicePixelRatio;
+    canvas.width = canvas.clientWidth * devicePixelRatio;
+    canvas.height = canvas.clientHeight * devicePixelRatio;
 
     context = canvas.getContext("webgpu");
+    if (!context) throw new Error("WebGPU context не создан");
     context.configure({
         device,
-        format,
+        format: navigator.gpu.getPreferredCanvasFormat(),
     });
+}
+
+async function main() {
+    await initWebGPU()
 
     const shaderCode = await fetch("shaders/triangle.wgsl")
         .then(response => response.text());
@@ -59,58 +62,23 @@ async function main() {
         code: shaderCode
     });
 
-    const vertices = new Float32Array([
-        0.0, 0.6,
-        -0.6, -0.6,
-        0.6, -0.6
-    ]);
-
-    vertexBuffer = device.createBuffer({
-        size: vertices.byteLength,
-        usage:
-            GPUBufferUsage.VERTEX |
-            GPUBufferUsage.COPY_DST
-    });
-
-    device.queue.writeBuffer(
-        vertexBuffer,
-        0,
-        vertices
-    );
+    const format = navigator.gpu.getPreferredCanvasFormat();
 
     pipeline = device.createRenderPipeline({
         layout: "auto",
-        vertex:
-        {
-            module: shaderModule,
-            entryPoint: "vertexMain",
-
-            buffers:
-                [
-                    {
-                        arrayStride: 8,
-                        attributes:
-                            [
-                                {
-                                    shaderLocation: 0,
-                                    offset: 0,
-                                    format: "float32x2"
-                                }
-                            ]
-                    }
-                ]
+        vertex: {
+            module: device.createShaderModule({
+                code: shaderCode,
+            }),
         },
 
-        fragment:
-        {
+        fragment: {
             module: shaderModule,
             entryPoint: "fragmentMain",
-
             targets: [{ format }]
         },
 
-        primitive:
-        {
+        primitive: {
             topology: "triangle-list"
         }
 
@@ -120,4 +88,4 @@ async function main() {
     render();
 }
 
-main();
+main().catch(console.error);
